217e3a61f28583a56595629c0cee142707c8234e
MattSchwartzkopf <schwartzkopfm@gmail.com>
2018-10-23 12:56:18 -0400

    Updated

--- Decorator Pattern/src/DouglasFir.java
+++ Decorator Pattern/src/DouglasFir.java
@@ -2,9 +2,9 @@
 public class DouglasFir extends Tree {
 
 	public DouglasFir() {
-		description = "Douglas Fir";
+		description = "Douglas Fir \n";
 	}
-	
+
 	public double cost() {
 		return 15.00;
 	}
