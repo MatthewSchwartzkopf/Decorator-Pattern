83881e4af6b005e2c99ce79933d42b3fc28e4067
MattSchwartzkopf <schwartzkopfm@gmail.com>
2018-10-23 12:57:11 -0400

    Updated

--- Decorator Pattern/src/BlueSpruce.java
+++ Decorator Pattern/src/BlueSpruce.java
@@ -2,9 +2,9 @@
 public class BlueSpruce extends Tree {
 
 	public BlueSpruce() {
-		description = "Colorado Blue Spruce";
+		description = "Colorado Blue Spruce \n";
 	}
-	
+
 	public double cost() {
 		return 20.00;
 	}
