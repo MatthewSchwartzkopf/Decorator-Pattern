5f2fc2a5c4f87292c8ed9e8579e491098227d1f6
Matthew Schwartzkopf <matthew.schwartzkopf@mymail.champlain.edu>
2018-10-23 12:19:21 -0400

    Added print() statements

--- Decorator Pattern/src/DecoratorTest.java
+++ Decorator Pattern/src/DecoratorTest.java
@@ -3,7 +3,7 @@ public class DecoratorTest {
 	public static void main(String[] args) {
 		
 		Tree blueSpruce = new BlueSpruce();
-		System.out.println(blueSpruce.getDescription() + " $" + blueSpruce.cost());
+		blueSpruce.print(blueSpruce);
 		
 		Tree balsamFir = new BalsamFir();
 		balsamFir = new Star(balsamFir);
@@ -14,19 +14,19 @@ public class DecoratorTest {
 		balsamFir = new Ribbons(balsamFir);
 		balsamFir = new Lights(balsamFir);
 		balsamFir = new LEDs(balsamFir);
-		System.out.println(balsamFir.getDescription() + " $" + balsamFir.cost());
+		balsamFir.print(balsamFir);
 		
 		Tree fraserFir = new FraserFir();
 		fraserFir = new Ruffles(fraserFir);
 		fraserFir = new Ribbons(fraserFir);
 		fraserFir = new Star(fraserFir);
-		System.out.println(fraserFir.getDescription() + " $" + fraserFir.cost());
+		fraserFir.print(fraserFir);
 		
 		Tree douglasFir = new DouglasFir();
 		douglasFir = new LEDs(douglasFir);
 		douglasFir = new BallsSilver(douglasFir);
 		douglasFir = new Ruffles(douglasFir);
 		douglasFir = new Star(douglasFir);
-		System.out.println(douglasFir.getDescription() + " $" + douglasFir.cost());
+		douglasFir.print(douglasFir);
 	}
 }
--- Decorator Pattern/src/Tree.java
+++ Decorator Pattern/src/Tree.java
@@ -1,10 +1,16 @@
 
 public abstract class Tree {
 	String description = "Unknown tree";
+	Tree tree;
 	
 	public String getDescription() {
 		return description;
 	}
 	
 	public abstract double cost();
+
+	public Tree print(Tree tree) {
+		System.out.println(tree.getDescription());
+		return tree;
+	}
 }
